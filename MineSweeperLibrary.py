af28f0907704b2c46d5314f21910f0ae2325a07a
Trey Simmons <tsimmons104@ivytech.edu>
2023-11-06 14:49:01 -0600

    V1.1

--- MineSweeperLibrary.py
+++ MineSweeperLibrary.py
@@ -17,6 +17,16 @@ import numpy
 Board_Preset_1_easy = [[-1, 3, 2, 1, 0],[2, -1, -1, 1, 0], [1, 2, 2, 1, 1], [ 1, 1, 2, -1, 1], [1, -1, 2, 1, 1]]
 #print(Board_Preset_1_easy[3][1])
 
+
+"Board_Preset_2_easy"
+"-1  2  1  0  0"
+" 3 -1  2  0  0"
+" 2 -1  2  0  0"
+" 2  2  1  0  0"
+"-1  1  0  0  0"
+Board_Preset_2_easy = [[-1, 2, 1, 0, 0],[3,-1, 2, 0, 0],[2,-1, 2, 0, 0],[2, 2, 1, 0, 0],[-1, 1, 0, 0, 0]]
+
+
 Board_Revealed_Preset_easy = [[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0]]
 
 
@@ -74,14 +84,14 @@ class MineSweeperBoard():
   Win_Loss_Flag = 0
 
   #"-This holds the board preset that was randomly chosen, after board object constructor this wil mainly be used for debug."
-  Board_Preset =  1
+  Board_Preset =  2
 
   #constructor
   #Chooses from the board presets (based on difficulty) and populates the board.
   def __init__(self):
     "#"
     #sets board preset, change later to choose from mulitple using case structure
-    self.board_Array = Board_Preset_1_easy.copy()
+    self.board_Array = Board_Preset_2_easy.copy()
     "#"
     #intializes the board_Array_revealed array to all 0's
     self.board_Array_Revealed = Board_Revealed_Preset_easy.copy()
@@ -92,8 +102,10 @@ class MineSweeperBoard():
   def __Board_Bombless_Clear(self, x, y):
      
      #debug stuff
-     print("recursive func enter" + str(x) + ", " + str(y))
+     #print("recursive func enter" + str(x) + ", " + str(y))
      #we are asssuming that the coordinates have already been converted into indexes
+
+     #x = rows, y = columns, kinda weird but its makes sense to me
 
      #reveals the current location
      self.board_Array_Revealed[x][y] = 1
@@ -109,39 +121,67 @@ class MineSweeperBoard():
      if(y - 1 < self.__board_column_length and y - 1 > -1 and self.board_Array[x][y -1] == 0 and self.board_Array_Revealed[x][y-1] != 1):#left
       self.__Board_Bombless_Clear(self, x, y -1)
 
-     if(y + 1 < self.__board_row_length and y + 1 > +1 and self.board_Array[x][y+1] == 0 and self.board_Array_Revealed[x][y+1] != 1):#right
+     if(y + 1 < self.__board_row_length and y + 1 > -1 and self.board_Array[x][y+1] == 0 and self.board_Array_Revealed[x][y+1] != 1):#right
       self.__Board_Bombless_Clear(self, x, y+1)
 
 
   #"-This function takes the players guess and reveals the specified square, If it is a bomb update the "
   #" Win_loss_Flag and return a -1, otherwise return the Bomb_data. If the Bomb_data == 0, call Board_Bombless_Clear(x, y)."
+  #takes coordinates based out of the top left
   def Player_Reveal_Guess(self, x, y):
-     #Adjusting indexes 
-     x = x-1 
-     y = y-1 
+     #Adjusting coords to indexes
+     row_index = y-1
+     col_index = x-1 
+      
 
     #reveals square 
-     self.board_Array_Revealed[x][y] = 1
+     self.board_Array_Revealed[row_index][col_index] = 1
 
     #checks for bomb
-     if(self.board_Array[x][y] == -1): #if there is a bomb
+     if(self.board_Array[row_index][col_index] == -1): #if there is a bomb
       #trips loss conditions
-      Win_Loss_Flag = -1
+      self.Win_Loss_Flag = -1
       return -1
      else: 
-        if(self.board_Array[x][y] == 0): # if there are no bombs adjacent, clear area on board
-           MineSweeperBoard.__Board_Bombless_Clear(self, x, y)
-           print("delete later")
+        if(self.board_Array[row_index][col_index] == 0): # if there are no bombs adjacent, clear area on board
+           MineSweeperBoard.__Board_Bombless_Clear(self, row_index, col_index)
 
         #if the guess was correct and there is no bomb there, return the number of bombs that are adjacent
-        return self.board_Array[x][y] 
-        
-        
+        return self.board_Array[row_index][col_index] 
+     
 
+   #this checks the board to see if the player has won or lost. First checks lost var, then increments through "
+   #the Board_Array to find if there are any non revealed non bomb squares left. If won returns 'win', lost returns 'loss',"
+   #no win or loss returns 'continue'
+  def Win_Lose_Check(self):
+   
+   #checks if loss condition flag has been tripped
+   if(self.Win_Loss_Flag == -1):
+     return "loss"
 
+   #index values
+   row_index = 0
+   col_index = 0
+   
 
-  
+   #iterates through the boards to look for a win condition, if all bombless squares are revealed then the player has won. 
+   while (row_index < self.__board_row_length):
+     while (col_index < self.__board_column_length):
+       print("row: " + str(row_index)  + ", col: " + str(col_index))
+       if(self.board_Array_Revealed[row_index][col_index] == 0 and self.board_Array[row_index][col_index] != -1):#if the current index location is both a non-bomb and is not revealed, return 'continue'
+         return "continue" #player has not won because they still need to reveal some non-bomb square
+       col_index = col_index + 1 # increments column
+      
+     #resets for next row 
+     col_index = 0
+     row_index =  row_index + 1 #increments row
+   
+   #win condition has been found 
+   Win_Loss_Flag =  1
+   return "win"
+      
 
+       
   #this is mainly used for debugging minsweeperBoard or debugging in general. All it does is print out both 
   #of the current boards
   def debug_board_console_print(self):
@@ -186,16 +226,6 @@ class MineSweeperBoard():
 
       
 
-    
-
-
-
-    
-
-
-  
-
-
 
 #test_list = [1][3]
 #print(test_list[0][0])
@@ -203,6 +233,7 @@ class MineSweeperBoard():
 
 #Class MineSweeperGame - this might be added to the minesweeper board class later
 "-This object encapsulates everything needed to keep track of the current game"
+
 "Player Score"
 
 "Board object"
--- MineSweeperTestMain.py
+++ MineSweeperTestMain.py
@@ -1,21 +1,40 @@
 import MineSweeperLibrary as MineSweeperBackEnd
 
-print("main")
 
 def main():
     print("main")
     dude = MineSweeperBackEnd.MineSweeperBoard()
     #test = dude.board_Array[4][4]
-    #print(test)
+    
+    
+
+    while(True):
+        
+        MineSweeperBackEnd.MineSweeperBoard.debug_board_console_print(dude)
+
+        print("Please enter a negative number to leave game. Otherwise just enter guess coordinates")
+
+        x_coord = int(input("X: "))
+        y_coord = int(input("Y: "))
+
+        #loop breakout 
+        if (x_coord < 0  or y_coord < 0):
+            break
+
+        MineSweeperBackEnd.MineSweeperBoard.Player_Reveal_Guess(dude, x_coord, y_coord)
+
+        #win output
+        print(MineSweeperBackEnd.MineSweeperBoard.Win_Lose_Check(dude))
+
+
+    
+
+    
 
     MineSweeperBackEnd.MineSweeperBoard.debug_board_console_print(dude)
 
-    print(MineSweeperBackEnd.MineSweeperBoard.Player_Reveal_Guess(dude, 1, 5))
-
-    MineSweeperBackEnd.MineSweeperBoard.debug_board_console_print(dude)
 
 
-
-#have this at the bottom of the page as thsi runs main 
+#have this at the bottom of the page as this runs main 
 if __name__ == "__main__":
     main()
